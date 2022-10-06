c3fe07fc76e3933eeaba13a2e724f7013e839c7c
Henrique Chagas <henriquehcoo@gmail.com>
2022-10-06 11:56:33 -0300

    rev1

---
+++ LAB7_3_2_2_PWM/LAB7_3_2_2_PWM.cydsn/codegentemp/LAB7_3_2_2_PWM.ctl
@@ -0,0 +1,9 @@
+-- ======================================================================
+-- LAB7_3_2_2_PWM.ctl generated from LAB7_3_2_2_PWM
+-- 10/06/2022 at 11:27
+-- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
+-- ======================================================================
+
+-- PSoC Clock Editor
+-- Directives Editor
+-- Analog Device Editor
--- LAB7_3_2_2_PWM/LAB7_3_2_2_PWM.cydsn/main.c
+++ LAB7_3_2_2_PWM/LAB7_3_2_2_PWM.cydsn/main.c
@@ -10,16 +10,89 @@
  * ========================================
 */
 #include "project.h"
+enum estados {ESTADO_20 = 1, ESTADO_40,ESTADO_60,ESTADO_70,ESTADO_80,ESTADO_100};
+enum estados led_state;
 
+   void go_to_state20(){
+        if (BT_Read() == 0){
+            led_state = ESTADO_20;
+            PWM_1_WriteCompare (200);
+
+        }
+}
+   void go_to_state40(){
+        if (BT_Read() == 0){
+            led_state = ESTADO_40;
+            PWM_1_WriteCompare (400);
+
+        }
+}
+   void go_to_state60(){
+        if (BT_Read() == 0){
+            led_state = ESTADO_60;
+            PWM_1_WriteCompare (600);
+
+        }
+}
+   void go_to_state70(){
+        if (BT_Read() == 0){
+            led_state = ESTADO_70;
+            PWM_1_WriteCompare (700);
+
+        }
+}
+   void go_to_state80(){
+        if (BT_Read() == 0){
+            led_state = ESTADO_80;
+            PWM_1_WriteCompare (800);
+
+        }
+}   void go_to_state100(){
+        if (BT_Read() == 0){
+            led_state = ESTADO_100;
+            PWM_1_WriteCompare (1000);
+
+        }
+}
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
+    PWM_1_Start();
+    Clock_Start();
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
     for(;;)
     {
-        /* Place your application code here. */
+    if (BT_Read() == 0){
+        
+        switch(led_state){
+        
+        case ESTADO_20:
+        go_to_state20();
+        break;
+         
+        case ESTADO_40:
+        go_to_state40();
+        break;
+        
+        case ESTADO_60:
+        go_to_state60();
+        break;
+         
+        case ESTADO_70:
+        go_to_state70();
+        break;
+        
+        case ESTADO_80:
+        go_to_state80();
+        break;
+        
+        case ESTADO_100:
+        go_to_state100();
+        break;
+        }
+        }
     }
 }
 
